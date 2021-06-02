7148326f09ae4789c792a297844ce8376484b9e2
João Lucas da Costa <63412716+LoopMon@users.noreply.github.com>
2021-06-02 10:05:07 -1100

    atualizando codigo

--- joao_lucas/main1.py
+++ joao_lucas/main1.py
@@ -1,13 +1,14 @@
-<<<<<<< HEAD
 import cv2
-from functions import *
+import joao_lucas.modules as func
+
+# DETECÇÃO DE BORDAS
 
 diretorio = './Imagens_a_serem_analisadas/'
 nome_img = 'eritrocitos_01'
 
 img = cv2.imread(diretorio + nome_img + '.jpg')
 
-img = resizeImg(img, 600)
+img = func.resizeImg(img, 600)
 
 grayScaleImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -21,7 +22,7 @@ contours, lx = cv2.findContours(cannyImg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NO
 celulas_vermelhas = []
 for i in contours:
     x, y, w, h = cv2.boundingRect(i)
-    ROI = img[y:y+(h), x:x+(w)]#pegar uma imagem do cortorno
+    ROI = img[y:y+(h), x:x+(w)] #pegar uma imagem do cortorno
 
     larguraR = int(ROI.shape[0] / 4)
     alturaR = int(ROI.shape[1] / 4)
@@ -30,46 +31,8 @@ for i in contours:
     if (larguraR > 10 and larguraR < 100) or (alturaR > 10 and alturaR < 100):
         celulas_vermelhas.append(i)
 
-cv2.drawContours(img, celulas_vermelhas, -1, (0,0,255), 2)
+cv2.drawContours(img, celulas_vermelhas, -1, (0,255,255), 2)
 
-showImage('Eritrocito', img)
-
-# cv2.imwrite(nome_img + '(copia).jpg', img)
-=======
-import cv2
-from functions import *
-
-diretorio = './Imagens_a_serem_analisadas/'
-nome_img = 'eritrocitos_01'
-
-img = cv2.imread(diretorio + nome_img + '.jpg')
-
-img = resizeImg(img, 600)
-
-grayScaleImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-blurImg = cv2.bilateralFilter(grayScaleImg, 9, 75, 75)
-cannyImg = cv2.Canny(blurImg, 10, 60)
-
-
-contours, lx = cv2.findContours(cannyImg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-
-celulas_vermelhas = []
-for i in contours:
-    x, y, w, h = cv2.boundingRect(i)
-    ROI = img[y:y+(h), x:x+(w)]#pegar uma imagem do cortorno
-
-    larguraR = int(ROI.shape[0] / 4)
-    alturaR = int(ROI.shape[1] / 4)
-
-    
-    if (larguraR > 10 and larguraR < 100) or (alturaR > 10 and alturaR < 100):
-        celulas_vermelhas.append(i)
-
-cv2.drawContours(img, celulas_vermelhas, -1, (0,0,255), 2)
-
-showImage('Eritrocito', img)
+func.showImage('Eritrocito', img)
 
 # cv2.imwrite(nome_img + '(copia).jpg', img)
->>>>>>> b7aecdf05d60ef96c9c2c9fe1dd6e19178dfaff9
